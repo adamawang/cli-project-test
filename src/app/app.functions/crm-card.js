3f6d736ae3bcf11520dbf35dc84141b8493a626d
Adam Wang <awang@hubspot.com>
2022-08-23 11:51:20 -0400

    Fix build

--- src/app/app.functions/crm-card.js
+++ src/app/app.functions/crm-card.js
@@ -37,7 +37,7 @@ exports.main = async (context = {}, sendResponse) => {
           {
             type: "text",
             format: "markdown",
-            text: `**Branch**: This is a develop branch build`
+            text: `**Branch**: This is a master branch build`
           }
         ]
       },
